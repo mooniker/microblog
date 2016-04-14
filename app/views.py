ac6a4b8bbda0eaba3de6b4198d30202eb1baed93
James Moon <mooniker@gmail.com>
2016-04-14 19:07:17 -0400

    Add loops for filler content to template

--- app/views.py
+++ app/views.py
@@ -5,4 +5,14 @@ from app import app
 @app.route('/index')
 def index():
     user = {'nickname': 'Miguel'} # placeholder user object for fake user
-    return render_template('index.html', title='Home', user=user)
+    posts = [ # placeholder/fake array of posts
+        {
+            'author': {'nickname': 'John'},
+            'body': 'Beautiful day in PDX!'
+        },
+        {
+            'author': {'nickname': 'Susan'},
+            'body': 'The Avengers movie was slick.'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
