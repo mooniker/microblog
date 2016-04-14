b9e510700829064d018340bde9f3cee3d3d0b7c0
James Moon <mooniker@gmail.com>
2016-04-14 18:51:07 -0400

    Add view and template for welcome page

--- app/views.py
+++ app/views.py
@@ -1,6 +1,8 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'Hello, World!'
+    user = {'nickname': 'Miguel'} # placeholder user object for fake user
+    return render_template('index.html', title='Home', user=user)
